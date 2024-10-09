9004220ee47c32d4fc9ddcd9a03e96767722801e
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-09 12:07:26 +0100

    feat: a visual of the navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,37 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <main className="bg-mint-green">
-      <header className="h-screen"></header>
+      <header
+        className="h-screen px-32 py-12"
+        style={{ fontFamily: "Montserrat, sans-serif" }}
+      >
+        <nav className="flex items-center justify-between w-full">
+          <figure className="text-[1.6rem]">
+            <Link to="/">Timmesafe</Link>
+          </figure>
+
+          <ul className="flex items-center gap-20 text-[1.3rem]">
+            <li>
+              <Link to="#works">How it works</Link>
+            </li>
+            <li>
+              <Link to="#works">Why Timmesafe</Link>
+            </li>
+            <li>
+              <button>Login</button>
+            </li>
+            <li>
+              <button>Sign Up</button>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </main>
   );
 }
 
 export default Home;
+
+// Montserrat
