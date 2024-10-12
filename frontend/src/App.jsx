a047fb11a80e9893c5a9bdc6babcf4f612c9853d
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-12 16:22:50 +0100

    feat: include the dynamic routing for the lock token page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />}>
             <Route index element={<LockToken />} />
+            <Route path="/lock-token" element={<LockToken />} />
           </Route>
         </Routes>
       </AuthContextProvider>
