8f69225df2ec17290166cecefeef697d629a32e5
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-12 13:34:02 +0100

    feat: add the dashboard options

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,8 +2,15 @@ import { Outlet } from "react-router-dom";
 
 function Dashboard() {
   return (
-    <main className="flex items-stretch">
-      <aside className="w-1/3">hello world</aside>
+    <main className="flex items-stretch min-h-screen">
+      <aside className="w-1/3 h-screen">
+        <ul>
+          <li>Lock Tokens</li>
+          <li>View Locked Tokens</li>
+          <li>Claim Tokens</li>
+          <li>Transfer Rights</li>
+        </ul>
+      </aside>
       <Outlet />
     </main>
   );
