399ee70d09179dd72ed401166617240b569e3bd2
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-12 04:26:08 +0100

    feat: initialize the connect wallet function

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,14 +1,38 @@
-import { createContext } from "react";
-import { AppConfig, UserSession } from "@stacks/connect";
+import { createContext, useState } from "react";
+import { AppConfig, showConnect, UserSession } from "@stacks/connect";
 
 const AuthProvider = createContext();
 
 function AuthContext({ children }) {
   const appConfig = new AppConfig(["store_write", "publish_data"]);
   const userSession = new UserSession({ appConfig });
-  console.log(userSession);
 
-  return <AuthProvider.Provider value={{}}>{children}</AuthProvider.Provider>;
+  // state
+  const [error, setError] = useState();
+
+  // function
+  const handleAuthenticate = function () {
+    showConnect({
+      appDetails: {
+        name: "Timmesafe",
+        icon: "",
+      },
+      redirectTo: "/hello",
+      onCancel: function () {
+        setError("It is closed");
+      },
+      onFinish: function () {
+        setError("");
+        console.log(userSession.loadUserData());
+      },
+    });
+  };
+
+  return (
+    <AuthProvider.Provider value={{ handleAuthenticate, error }}>
+      {children}
+    </AuthProvider.Provider>
+  );
 }
 
 export default AuthContext;
