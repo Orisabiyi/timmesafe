f39a3964a06311c4ad1f777653f73fbb6ccd68ce
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-14 13:48:59 +0100

    style: style the section of the Lock Token page

--- frontend/src/pages/ViewLockTokens.jsx
+++ frontend/src/pages/ViewLockTokens.jsx
@@ -1,5 +1,9 @@
 function ViewLockTokens() {
-  return <section>Hello World</section>;
+  return (
+    <section className="flex-1 px-10 py-16 bg-light-gray flex flex-col text-dark-gray">
+      Hello World
+    </section>
+  );
 }
 
 export default ViewLockTokens;
