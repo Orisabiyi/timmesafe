73e55f74d31ed1c12ae58f7cd07bf74472e1f747
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-14 13:39:34 +0100

    feat: Add route for the locked-tokens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ import { Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import { AuthContextProvider } from "./contexts/AuthContext";
+
 import Dashboard from "./pages/Dashboard";
 import LockToken from "./pages/LockToken";
+import ViewLockTokens from "./pages/ViewLockTokens";
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />}>
             <Route index element={<LockToken />} />
             <Route path="lock-token" element={<LockToken />} />
+            <Route path="locked-token" element={<ViewLockTokens />} />
           </Route>
         </Routes>
       </AuthContextProvider>
