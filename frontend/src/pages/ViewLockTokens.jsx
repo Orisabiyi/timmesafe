2ab788d23c48517bb701e7ec958176694b64fe5b
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-14 15:42:33 +0100

    style: update the page styling of ViewLockTokens component

--- frontend/src/pages/ViewLockTokens.jsx
+++ frontend/src/pages/ViewLockTokens.jsx
@@ -1,10 +1,23 @@
 function ViewLockTokens() {
   return (
     <section className="flex-1 px-10 py-16 bg-light-gray flex flex-col text-dark-gray">
-      <ul className="flex flex-col w-full">
-        <li>Hello World</li>
-        <li></li>
-        <li></li>
+      <ul className="flex flex-col gap-4 w-full text-13">
+        {[...Array(11)].map((_, i) => (
+          <li key={i} className="flex gap-4 border-b-mint-green border-b-2 p-4">
+            <span>
+              <b className="font-medium">Beneficiary:</b>{" "}
+              {sessionStorage.address}
+            </span>
+            <span>|</span>
+            <span>
+              <b className="font-medium">Token Locked:</b> 2000 stx
+            </span>
+
+            <button className="ml-auto bg-dark-mint-green w-10 h-10 rounded-full flex items-center justify-center">
+              +
+            </button>
+          </li>
+        ))}
       </ul>
     </section>
   );
